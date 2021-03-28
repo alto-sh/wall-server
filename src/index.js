d08db7797d35568cf550d2dff826573294bfb5b8
Solomon Jackson <sugarfi@sugarfi.dev>
2021-03-28 16:09:04 -0400

    rework rendering code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const fetch = require('node-fetch');
 
 const app = express();
 const http = require('http').createServer(app);
@@ -8,6 +9,8 @@ const io = require('socket.io')(http, {
         origin: '*'
     }
 });
+
+const URL = 'http://localhost:8080';
 
 const urls = {};
 const history = [];
@@ -27,6 +30,34 @@ app.get('/canvas/:url', (req, res) => {
             data: ''
         });
     }
+});
+
+app.get('/fetch/:url', (req, res) => {
+    let url = req.params.url.replace(/^\/\//, '');
+    url = url.startsWith('http') ? url : 'http://' + url;
+    const base = url.match(/^https?:\/\/([a-zA-Z0-9-]+\.)+[a-zA-Z0-9-]+/)[0];
+    fetch(url).then(r => {
+        const mime = r.headers.get('Content-Type').split(';')[0];
+        res.setHeader('Content-Type', mime);
+        if (mime.startsWith('text')) {
+            r.text().then(text => {
+                const data = text
+                    .replace(/(src|href)="(.*?)"/g, (_, attr, val) => {
+                        const path = val.match(/^(\/?[a-zA-Z0-9_%.-])+$/) ? base + '/' + val : val;
+                        return (
+                            `${attr}="${URL}/fetch/${encodeURIComponent(path)}"`
+                        );
+                    });
+                res.send(data);
+            });
+        } else {
+            r.blob().then(blob => {
+                blob.arrayBuffer().then(buf => {
+                    res.send(Buffer.from(buf));
+                });
+            });
+        }
+    });
 });
 
 app.get('/history', (req, res) => {
